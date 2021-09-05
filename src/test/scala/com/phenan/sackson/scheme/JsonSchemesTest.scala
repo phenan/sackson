b96265667dddb6fb0bdcc299e23c08e4c81af64f
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2021-09-05 22:33:25 +0900

    fix JsonSchemesTest

--- src/test/scala/com/phenan/sackson/scheme/JsonSchemesTest.scala
+++ src/test/scala/com/phenan/sackson/scheme/JsonSchemesTest.scala
@@ -11,12 +11,6 @@ import java.io.ByteArrayOutputStream
 class JsonSchemesTest extends AnyWordSpec {
   private val jsonFactory = new JsonFactory()
 
-  private def runParser[T](parser: JsonParser[T], string: String): Either[JsonParseError, T] = {
-    val jsonParser = jsonFactory.createParser(string).nn
-    jsonParser.nextToken()
-    parser.run(jsonParser, JsonPath.Root, JsonParser.Options())
-  }
-
   private def runPrinter[T](printer: JsonPrinter[T], value: T): String = {
     val stream = new ByteArrayOutputStream()
     val generator = jsonFactory.createGenerator(stream).nn
@@ -37,10 +31,10 @@ class JsonSchemesTest extends AnyWordSpec {
 
   "singleFieldStruct.parser" should {
     "parse empty object as (None)" in {
-      assert(runParser(singleFieldStruct.parser, """{}""") == Right(Tuple(None)))
+      assert(singleFieldStruct.parser.parseString("""{}""") == Right(Tuple(None)))
     }
     "parse single field object as single value tuple" in {
-      assert(runParser(singleFieldStruct.parser, """{"b":true}""") === Right(Tuple(Some(true))))
+      assert(singleFieldStruct.parser.parseString("""{"b":true}""") === Right(Tuple(Some(true))))
     }
   }
 
@@ -61,10 +55,10 @@ class JsonSchemesTest extends AnyWordSpec {
 
   "structWith2Fields.parser" should {
     "parse single field object as (true, None)" in {
-      assert(runParser(structWith2Fields.parser, """{"b1":true}""") == Right((true, None)))
+      assert(structWith2Fields.parser.parseString("""{"b1":true}""") == Right((true, None)))
     }
     "parse two fields object as (true, Some(false))" in {
-      assert(runParser(structWith2Fields.parser, """{"b1":true,"b2":false}""") == Right((true, Some(false))))
+      assert(structWith2Fields.parser.parseString("""{"b1":true,"b2":false}""") == Right((true, Some(false))))
     }
   }
 
