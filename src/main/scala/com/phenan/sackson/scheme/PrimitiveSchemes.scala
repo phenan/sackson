4aa9e4277456370d6f29d1e133d56f13d5f35f1c
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2021-09-06 21:33:19 +0900

    implement schema for primitive schemes

--- src/main/scala/com/phenan/sackson/scheme/PrimitiveSchemes.scala
+++ src/main/scala/com/phenan/sackson/scheme/PrimitiveSchemes.scala
@@ -9,24 +9,27 @@ object PrimitiveSchemes {
   object BooleanScheme extends JsonScheme[Boolean] {
     override def parser: JsonParser[Boolean] = PrimitiveParsers.BooleanParser
     override def printer: JsonPrinter[Boolean] = PrimitivePrinters.BooleanPrinter
-    override def schema: JsonPrinter[Unit] = ???/*struct(
+    override def schema: JsonPrinter[Unit] = struct(
       required("type", string.const("boolean"))
       *: nil
-    ).printer.contraMap((_: Unit) => Tuple(()))*/
+    ).printer.contraMap((_: Unit) => Tuple(()))
   }
 
   object IntScheme extends JsonScheme[Int] {
     override def parser: JsonParser[Int] = PrimitiveParsers.IntParser
     override def printer: JsonPrinter[Int] = PrimitivePrinters.IntPrinter
-    override def schema: JsonPrinter[Unit] = ???/*struct(
+    override def schema: JsonPrinter[Unit] = struct(
       required("type", string.const("number"))
       *: nil
-    ).printer.contraMap((_: Unit) => Tuple(()))*/
+    ).printer.contraMap((_: Unit) => Tuple(()))
   }
-  
+
   object StringScheme extends JsonScheme[String] {
     override def parser: JsonParser[String] = PrimitiveParsers.StringParser
     override def printer: JsonPrinter[String] = PrimitivePrinters.StringPrinter
-    override def schema: JsonPrinter[Unit] = ???
+    override def schema: JsonPrinter[Unit] = struct(
+      required("type", string.const("string"))
+        *: nil
+    ).printer.contraMap((_: Unit) => Tuple(()))
   }
 }
